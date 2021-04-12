7ad9f88b3bea1b6a3300298958e38bfdc0be132d
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-04-12 12:14:19 +0300

    dinamic create winwow

---
+++ plagin/modal.js
@@ -0,0 +1,40 @@
+function _createModal(options) {
+    const modal = document.createElement('div');
+    modal.classList.add('vmodal');
+    modal.insertAdjacentHTML('afterBegin',
+    ` 
+    <div class="modal-overlay">
+       <div class="modal-window">
+            <div class="modal-header">
+                <span class="modal-title"> Modal title </span>
+                <span class="modal-close">&times;</span>
+            </div>
+            <div class="modal-body">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="modal-footer">
+                <button>Ok</button>
+                <button>Cancel</button>
+            </div>
+        </div>
+    </div>
+     
+    `
+    );
+    //insertAdjacentHTML end
+    document.body.appendChild(modal);
+    return modal
+    
+}
+
+$.modal = function (options) {
+    // знак  $ показывает что в переменной node элемент 
+    const $modal = _createModal(options)
+
+    return {
+        open(){},
+        close(){},
+        destroy(){}
+     }    
+}
