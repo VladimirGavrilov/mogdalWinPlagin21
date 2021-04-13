6b703a76ca13c2298d74326272a8a9735cc37237
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-04-13 12:25:13 +0300

    add dinamic content

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
+//показать цену в модалке
+const deserts = [
+    {   id: 1,
+        prise: 20,
+        title: 'Пироженое', 
+        image: 'https://s2.best-wallpaper.net/wallpaper/1024x768/1308/Dessert-cake-strawberries-sweet-food_1024x768.jpg',        
+        text:'Dessert Kuchen, Erdbeeren, süße Speisen'
+    },
+    {
+        id: 2,
+        prise: 70,
+        title: ' Мороженое', 
+        image: 'https://apelsin-plus.com.ua/files/import/755733554_lozhka-dlya-morozhenogo.jpg',
+        text: 'Фруктовый салат с мороженым'
+    },
+    {
+        id: 3,
+        prise: 120,
+        title: 'Пироженое',
+        image: 'https://s2.best-wallpaper.net/wallpaper/1024x768/1308/Dessert-cake-strawberries-sweet-food_1024x768.jpg',
+        text: 'Dessert Kuchen, Erdbeeren, süße Speisen'
+    }
+]
+const toHTML = (desert) => 
+    `<div class="col">
+            <div class="card">
+                <img src="${desert.image}"
+                    class="card-img-top" style="height: 300px; " alt="${ desert.title }">
+                <div class="card-body">
+                    <h5 class="card-title">${ desert.title }</h5>
+                    <p class="card-text">${ desert.text}</p>
+                    <a href="#" class="btn btn-primary" data-btn = "price">Посмотреть цену</a>
+                    <a href="#" class="btn btn-danger">Удалить</a>
+        </div>
+    </div>`
+
+
+function render() {
+    const html = deserts.map(toHTML ).join('')
+    document.getElementById('desert').innerHTML = html
+}
+render()
 const modal = $.modal({
     title: 'Титул окна!',
     closable: true,
     content: `
-        <p>Контент модального окна!</p>
+        <>Контент модального окна!</>
     
     `,
     width: '300px',
@@ -29,3 +71,11 @@ const modal = $.modal({
 
 }
 );
+document.addEventListener('click', event => {
+    const btnType = event.target.dataset.btn
+    // console.log( 'Clict', event.target.dataset.close )
+    if (btnType == 'price') {
+        modal.open()
+
+    }
+})
